4e2413317e59d71a2ee669db66cd88b7b36220a0
wo2ek8 <jae2son@gmail.com>
2022-04-01 17:39:25 +0900

    41

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
+import "./App.css";
+import Navigation from "./components/Navigation";
 
-class App extends React.Component {
-  state = {
-    count: 0,
-  };
-  add = () => {
-    console.log("add");
-  };
-  minus = () => {
-    console.log("minus");
-  };
-  render() {
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/movie/:id" component={Detail} />
+    </HashRouter>
+  );
 }
 
 export default App;
